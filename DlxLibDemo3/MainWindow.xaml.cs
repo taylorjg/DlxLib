637ce7771334f4b64a7566b4a96a9ed8d1b5b3c4
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-08-03 23:29:18 +0100

    Started to move towards MVVM - add a MainWindowViewModel class

--- DlxLibDemo3/MainWindow.xaml.cs
+++ DlxLibDemo3/MainWindow.xaml.cs
@@ -1,112 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Windows.Threading;
-using DlxLibDemo3.Annotations;
-using DlxLibDemo3.Model;
+﻿using DlxLibDemo3.ViewModel;
 
 namespace DlxLibDemo3
 {
-    public partial class MainWindow : INotifyPropertyChanged
+    public partial class MainWindow
     {
-        private int _iterations;
-        private int _interval;
-        private readonly Solver _solver = new Solver(Pieces.ThePieces, 8);
-        private readonly DispatcherTimer _timer = new DispatcherTimer();
-
-        public int Iterations
-        {
-            get { return _iterations; }
-            private set
-            {
-                if (value == _iterations) return;
-                _iterations = value;
-                OnPropertyChanged("Iterations");
-            }
-        }
-
-        public int Interval
-        {
-            get { return _interval; }
-            private set
-            {
-                if (value == _interval) return;
-                _interval = value;
-                OnPropertyChanged("Interval");
-                _timer.Interval = TimeSpan.FromMilliseconds(value);
-            }
-        }
-
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
 
-            Iterations = 0;
-            Interval = 10;
+            // Obviously, we don't want to pass BoardControl to MainWindowViewModel.
+            // Need to figure out a proper MVVM solution to this.
+            // We could add a BoardControlViewModel and pass that in instead ?
+            // Perhaps BoardControlViewModel could expose a PieceDetails property which we could set.
+            // Then, in the setter for the PieceDetails property in BoardControlViewModel, we could
+            // perform the code that is currently in MainWindowViewModel.ProcessSearchStep ?
+            DataContext = new MainWindowViewModel(BoardControl);
 
             ContentRendered += (_, __) => BoardControl.DrawGrid();
-            Closing += (_, __) => _solver.Cancel();
-
-            _timer.Tick += (_, __) =>
-                {
-                    IEnumerable<Tuple<RotatedPiece, int, int>> pieceDetails;
-                    if (_solver.SearchSteps.TryDequeue(out pieceDetails))
-                    {
-                        var pieceDetailsList = pieceDetails.ToList();
-                        ProcessSearchStep(pieceDetailsList);
-                        if (pieceDetailsList.Count == Pieces.ThePieces.Count())
-                        {
-                            _timer.Stop();
-                        }
-                    }
-                };
-
-            _solver.Solve();
-            _timer.Start();
-        }
-
-        private void ProcessSearchStep(IList<Tuple<RotatedPiece, int, int>> pieceDetails)
-        {
-            Iterations++;
-
-            foreach (var pd in pieceDetails)
-            {
-                var rotatedPiece = pd.Item1;
-                var pieceName = rotatedPiece.Piece.Name;
-                var orientation = rotatedPiece.Orientation;
-                var x = pd.Item2;
-                var y = pd.Item3;
-
-                if (BoardControl.IsPieceOnBoard(pieceName))
-                {
-                    if (!BoardControl.IsPieceOnBoardWithOrientationandLocation(pieceName, orientation, x, y))
-                    {
-                        BoardControl.RemovePiece(pieceName);
-                        BoardControl.AddPiece(rotatedPiece, x, y);
-                    }
-                }
-                else
-                {
-                    BoardControl.AddPiece(rotatedPiece, x, y);
-                }
-            }
-
-            BoardControl.RemovePiecesOtherThan(pieceDetails.Select(pd => pd.Item1.Piece.Name).ToList());
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            var handler = PropertyChanged;
-
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
+            Closing += (_, __) => ((MainWindowViewModel) DataContext).Closing();
         }
     }
 }
---
+++ DlxLibDemo3/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Threading;
+using DlxLibDemo3.Annotations;
+using DlxLibDemo3.Model;
+
+namespace DlxLibDemo3.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly BoardControl _boardControl;
+        private int _iterations;
+        private int _interval;
+        private readonly Solver _solver = new Solver(Pieces.ThePieces, 8);
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+
+        public MainWindowViewModel(BoardControl boardControl)
+        {
+            _boardControl = boardControl;
+            Iterations = 0;
+            Interval = 10;
+
+            _timer.Tick += (_, __) =>
+            {
+                IEnumerable<Tuple<RotatedPiece, int, int>> pieceDetails;
+                if (_solver.SearchSteps.TryDequeue(out pieceDetails))
+                {
+                    var pieceDetailsList = pieceDetails.ToList();
+                    ProcessSearchStep(pieceDetailsList);
+                    if (pieceDetailsList.Count == Pieces.ThePieces.Count())
+                    {
+                        _timer.Stop();
+                    }
+                }
+            };
+
+            _solver.Solve();
+            _timer.Start();
+        }
+
+        private void ProcessSearchStep(IList<Tuple<RotatedPiece, int, int>> pieceDetails)
+        {
+            Iterations++;
+
+            foreach (var pd in pieceDetails)
+            {
+                var rotatedPiece = pd.Item1;
+                var pieceName = rotatedPiece.Piece.Name;
+                var orientation = rotatedPiece.Orientation;
+                var x = pd.Item2;
+                var y = pd.Item3;
+
+                if (_boardControl.IsPieceOnBoard(pieceName))
+                {
+                    if (!_boardControl.IsPieceOnBoardWithOrientationandLocation(pieceName, orientation, x, y))
+                    {
+                        _boardControl.RemovePiece(pieceName);
+                        _boardControl.AddPiece(rotatedPiece, x, y);
+                    }
+                }
+                else
+                {
+                    _boardControl.AddPiece(rotatedPiece, x, y);
+                }
+            }
+
+            _boardControl.RemovePiecesOtherThan(pieceDetails.Select(pd => pd.Item1.Piece.Name).ToList());
+        }
+
+        public void Closing()
+        {
+            _solver.Cancel();
+        }
+
+        public int Iterations
+        {
+            get { return _iterations; }
+            private set
+            {
+                if (value == _iterations) return;
+                _iterations = value;
+                OnPropertyChanged("Iterations");
+            }
+        }
+
+        public int Interval
+        {
+            get { return _interval; }
+            private set
+            {
+                if (value == _interval) return;
+                _interval = value;
+                OnPropertyChanged("Interval");
+                _timer.Interval = TimeSpan.FromMilliseconds(value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
