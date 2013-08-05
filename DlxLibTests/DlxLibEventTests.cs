28f37c6d5ad1306e35b1f0048df7e565e1dd713c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-08-05 23:46:39 +0100

    Add unit tests around the DlxLib events

--- DlxLibTests/DlxLibEventTests.cs
+++ DlxLibTests/DlxLibEventTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using DlxLib;
+using NUnit.Framework;
 
 namespace DlxLibTests
 {
@@ -8,26 +9,75 @@ namespace DlxLibTests
         [Test]
         public void StartedEventFires()
         {
+            var matrix = new bool[0, 0];
+            var dlx = new Dlx();
+            var startedEventHasBeenRaised = false;
+            dlx.Started += (_, __) => startedEventHasBeenRaised = true;
+            dlx.Solve(matrix);
+            Assert.That(startedEventHasBeenRaised, Is.True, "Expected the Started event to have been raised");
         }
 
         [Test]
         public void FinishedEventFires()
         {
+            var matrix = new bool[0, 0];
+            var dlx = new Dlx();
+            var finishedEventHasBeenRaised = false;
+            dlx.Finished += (_, __) => finishedEventHasBeenRaised = true;
+            dlx.Solve(matrix);
+            Assert.That(finishedEventHasBeenRaised, Is.True, "Expected the Finished event to have been raised");
         }
 
         [Test]
         public void CancelledEventFires()
         {
+            var matrix = new bool[0, 0];
+            var dlx = new Dlx();
+            var cancelledEventHasBeenRaised = false;
+            dlx.Cancelled += (_, __) => cancelledEventHasBeenRaised = true;
+            dlx.Started += (sender, __) => ((Dlx)sender).Cancel();
+            var thread = new System.Threading.Thread(() => dlx.Solve(matrix));
+            thread.Start();
+            thread.Join();
+            Assert.That(cancelledEventHasBeenRaised, Is.True, "Expected the Cancelled event to have been raised");
         }
 
         [Test]
         public void SearchStepEventFires()
         {
+            var matrix = new[,]
+                {
+                    {0, 0, 1, 0, 1, 1, 0},
+                    {1, 0, 0, 1, 0, 0, 1},
+                    {0, 1, 1, 0, 0, 1, 0},
+                    {1, 0, 0, 1, 0, 0, 0},
+                    {0, 1, 0, 0, 0, 0, 1},
+                    {0, 0, 0, 1, 1, 0, 1}
+                };
+            var dlx = new Dlx();
+            var searchStepEventHasBeenRaised = false;
+            dlx.SearchStep += (_, __) => searchStepEventHasBeenRaised = true;
+            dlx.Solve(matrix);
+            Assert.That(searchStepEventHasBeenRaised, Is.True, "Expected the SearchStep event to have been raised");
         }
 
         [Test]
         public void SolutionFoundEventFires()
         {
+            var matrix = new[,]
+                {
+                    {0, 0, 1, 0, 1, 1, 0},
+                    {1, 0, 0, 1, 0, 0, 1},
+                    {0, 1, 1, 0, 0, 1, 0},
+                    {1, 0, 0, 1, 0, 0, 0},
+                    {0, 1, 0, 0, 0, 0, 1},
+                    {0, 0, 0, 1, 1, 0, 1}
+                };
+            var dlx = new Dlx();
+            var solutionFoundEventHasBeenRaised = false;
+            dlx.SolutionFound += (_, __) => solutionFoundEventHasBeenRaised = true;
+            dlx.Solve(matrix);
+            Assert.That(solutionFoundEventHasBeenRaised, Is.True, "Expected the SolutionFound event to have been raised");
         }
     }
 }
