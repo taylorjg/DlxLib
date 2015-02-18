7f1e4305f9833df85ebdcbec7df168b41d59bac3
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-18 12:24:39 +0000

    Started to add property-based tests using FsCheck

---
+++ DlxLibPropertyTests/FsCheckAddin.cs
@@ -0,0 +1,16 @@
+﻿using FsCheck.NUnit.Addin;
+using NUnit.Core.Extensibility;
+
+namespace DlxLibPropertyTests
+{
+    [NUnitAddin(Description = "FsCheck addin")]
+    public class FsCheckAddin : IAddin
+    {
+        public bool Install(IExtensionHost host)
+        {
+            var tcBuilder = new FsCheckTestCaseBuider();
+            host.GetExtensionPoint("TestCaseBuilders").Install(tcBuilder);
+            return true;
+        }
+    }
+}
---
+++ DlxLibPropertyTests/PropertyTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using DlxLib;
+using FsCheck;
+using FsCheck.Fluent;
+using NUnit.Framework;
+
+namespace DlxLibPropertyTests
+{
+    using Property = Gen<Rose<Result>>;
+    
+    [TestFixture]
+    public class PropertyTests
+    {
+        [FsCheck.NUnit.Property]
+        public Property ExactCoverProblemsWithNoSolutions()
+        {
+            return Spec
+                .For(GenMatrixWithNoSolutions(Any.OfType<int>()), matrix => !new Dlx().Solve(matrix).Any())
+                .Build();
+        }
+
+        [FsCheck.NUnit.Property]
+        public Property ExactCoverProblemsWithSingleSolution()
+        {
+            return Spec
+                .For(GenMatrixWithSingleSolution(Any.OfType<int>()), matrix => new Dlx().Solve(matrix).Count() == 1)
+                .Build();
+        }
+
+        [FsCheck.NUnit.Property]
+        public Property ExactCoverProblemsWithMultipleSolutions(int nParam)
+        {
+            return Spec
+                .For(Any.Value(nParam), GenMatrixWithMultipleSolutions(Any.OfType<int>(), nParam), (n, matrix) => new Dlx().Solve(matrix).Count() == n)
+                .When((n, _) => n > 1)
+                .Build();
+        }
+
+        private static Gen<T[,]> GenMatrixWithNoSolutions<T>(Gen<T> genT)
+        {
+            return Any.OfType<T[,]>();
+        }
+        private static Gen<T[,]> GenMatrixWithSingleSolution<T>(Gen<T> genT)
+        {
+            return Any.OfType<T[,]>();
+        }
+
+        private static Gen<T[,]> GenMatrixWithMultipleSolutions<T>(Gen<T> genT, int n)
+        {
+            return Any.OfType<T[,]>();
+        }
+    }
+}
