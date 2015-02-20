e461a6a18d7e35e1d276952a2f45671288cfe2f5
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-20 14:54:29 +0000

    Tweaks to the first property test

---
+++ DlxLibPropertyTests/JaggedArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DlxLibPropertyTests
+{
+    public static class JaggedArrayExtensions
+    {
+        public static T[,] To2DArray<T>(this IReadOnlyList<IReadOnlyList<T>> jaggedArray)
+        {
+            var numRows = jaggedArray.Count;
+            var numCols = jaggedArray[0].Count;
+
+            var twoDimensionalArray = new T[numRows, numCols];
+
+            for (var row = 0; row < numRows; row++)
+            {
+                for (var col = 0; col < numCols; col++)
+                {
+                    twoDimensionalArray[row, col] = jaggedArray[row][col];
+                }
+            }
+
+            return twoDimensionalArray;
+        }
+    }
+}
--- DlxLibPropertyTests/PropertyTests.cs
+++ DlxLibPropertyTests/PropertyTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using DlxLib;
 using FsCheck;
 using FsCheck.Fluent;
@@ -38,18 +39,25 @@ namespace DlxLibPropertyTests
 
         private static Gen<int[,]> GenMatrixOfIntWithNoSolutions()
         {
+            var zeroOrOne = Any.ValueIn(0, 1);
+
             return
-                from numRows in Any.IntBetween(20, 100)
-                from numCols in Any.IntBetween(4, 100)
-                let genRowsA = Any.Value(Enumerable.Repeat(0, numCols).ToArray())
+                from numRows in Any.IntBetween(2, 100)
+                from numCols in Any.IntBetween(2, 500)
+                let allZeroes = Enumerable.Repeat(0, numCols).ToList()
+                let genRowsA = Any.Value(allZeroes)
+                from indexOfAlwaysZeroColumn in Any.IntBetween(0, numCols - 1)
+                let prefixLength = indexOfAlwaysZeroColumn
+                let suffixLength = numCols - indexOfAlwaysZeroColumn - 1
                 let genRowsB =
-                    from numOnes in Any.IntBetween(1, numCols - 1)
-                    select Enumerable.Repeat(1, numOnes).Concat(Enumerable.Repeat(0, numCols - numOnes)).ToArray()
+                    from prefix in zeroOrOne.MakeListOfLength(prefixLength)
+                    from suffix in zeroOrOne.MakeListOfLength(suffixLength)
+                    select prefix.Concat(new[]{0}).Concat(suffix).ToList()
                 let genRows = Any.WeighedGeneratorIn(
-                    new WeightAndValue<Gen<int[]>>(90, genRowsA),
-                    new WeightAndValue<Gen<int[]>>(10, genRowsB))
-                from rows in genRows.MakeListOfLength(numRows).Select(x => x.ToArray())
-                select JaggedArrayTo2DArray(rows);
+                    new WeightAndValue<Gen<List<int>>>(90, genRowsA),
+                    new WeightAndValue<Gen<List<int>>>(10, genRowsB))
+                from rows in genRows.MakeListOfLength(numRows)
+                select rows.To2DArray();
         }
 
         //private static Gen<int[,]> GenMatrixOfIntWithSingleSolution()
@@ -61,23 +69,5 @@ namespace DlxLibPropertyTests
         //{
         //    return Any.OfType<int[,]>();
         //}
-
-        private static T[,] JaggedArrayTo2DArray<T>(T[][] jaggedArray)
-        {
-            var numRows = jaggedArray.Length;
-            var numCols = jaggedArray[0].Length;
-
-            var twoDimensionalArray = new T[numRows, numCols];
-
-            for (var row = 0; row < numRows; row++)
-            {
-                for (var col = 0; col < numCols; col++)
-                {
-                    twoDimensionalArray[row, col] = jaggedArray[row][col];
-                }
-            }
-
-            return twoDimensionalArray;
-        }
     }
 }
