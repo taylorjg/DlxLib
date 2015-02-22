1141f2a2422d6593b5c76e572dbfd6dea9099578
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-22 22:08:41 +0000

    Simplified the generate of matrices with no solutions

--- DlxLibPropertyTests/PropertyTests.cs
+++ DlxLibPropertyTests/PropertyTests.cs
@@ -14,7 +14,7 @@ namespace DlxLibPropertyTests
     [TestFixture]
     public class PropertyTests
     {
-        [FsCheck.NUnit.Property]
+        [FsCheck.NUnit.Property(Verbose = false)]
         public Property ExactCoverProblemsWithNoSolutions()
         {
             return Spec
@@ -22,70 +22,19 @@ namespace DlxLibPropertyTests
                 .Build();
         }
 
-        //[FsCheck.NUnit.Property(Verbose = true, MaxTest = 10)]
-        //public Property ExactCoverProblemsWithSingleSolution()
-        //{
-        //    return Spec
-        //        .For(GenMatrixOfIntWithSingleSolution(), matrix => new Dlx().Solve(matrix).Count() == 1)
-        //        .Build();
-        //}
-
-        [FsCheck.NUnit.Property(Verbose = true, MaxTest = 10)]
-        public Property GenPieceLengthsSample()
+        [FsCheck.NUnit.Property(Verbose = false)]
+        public Property ExactCoverProblemsWithSingleSolution()
         {
-            var gen = GenPieceLengths(18, 3);
-            var samples = Gen.sample(10, 10, gen);
-            foreach (var sample in samples) Dump("GenPieceLengths sample: {0}", SequenceToString(sample));
-            return Prop.ofTestable(true);
-        }
-
-        [FsCheck.NUnit.Property(Verbose = true)]
-        public Property GenPartialSolutionRowsSample()
-        {
-            var gen = GenPartialSolutionRows(18, 5);
-            var samples = Gen.sample(10, 10, gen);
-
-            var sampleIndex = 0;
-            foreach (var sample in samples)
-            {
-                var rowIndex = 0;
-                foreach (var row in sample)
+            return Spec
+                .For(GenMatrixOfIntWithSingleSolution(), matrix =>
                 {
-                    Dump("sampleIndex: {0}; rowIndex: {1}; sample: {2}", sampleIndex, rowIndex, SequenceToString(row));
-                    rowIndex++;
-                }
-                sampleIndex++;
-            }
-
-            return Prop.ofTestable(true);
+                    var solutions = new Dlx().Solve(matrix).ToList();
+                    return solutions.Count == 1 && CheckSolution(solutions.First(), matrix);
+                })
+                .Build();
         }
 
-        [FsCheck.NUnit.Property(Verbose = true)]
-        public Property GenMatrixOfIntWithSingleSolutionSample()
-        {
-            var gen = GenMatrixOfIntWithSingleSolution();
-            var samples = Gen.sample(10, 10, gen);
-
-            var sampleIndex = 0;
-            foreach (var sample in samples)
-            {
-                for (var rowIndex = 0; rowIndex < sample.GetLength(0); rowIndex++)
-                {
-                    var rowValues = new List<int>();
-                    for (var colIndex = 0; colIndex < sample.GetLength(1); colIndex++)
-                    {
-                        rowValues.Add(sample[rowIndex, colIndex]);
-                    }
-                    Dump("sampleIndex: {0}; row[{1}]: {2}", sampleIndex, rowIndex, SequenceToString(rowValues));
-                }
-                Dump("\n");
-                sampleIndex++;
-            }
-
-            return Prop.ofTestable(true);
-        }
-
-        //[FsCheck.NUnit.Property]
+        //[FsCheck.NUnit.Property(Verbose = false)]
         //public Property ExactCoverProblemsWithMultipleSolutions(int nParam)
         //{
         //    return Spec
@@ -94,39 +43,44 @@ namespace DlxLibPropertyTests
         //        .Build();
         //}
 
-        // TODO: add a helper method to check each solution...
+        private static bool CheckSolution(Solution solution, int[,] matrix)
+        {
+            var numCols = matrix.GetLength(1);
+            var numOnes = 0;
+            var numZeros = 0;
+
+            foreach (var rowIndex in solution.RowIndexes)
+            {
+                for (var colIndex = 0; colIndex < numCols; colIndex++)
+                {
+                    if (matrix[rowIndex, colIndex] == 1) numOnes++;
+                    if (matrix[rowIndex, colIndex] == 0) numZeros++;
+                }
+            }
+
+            return (numOnes == numCols) && (numOnes + numZeros == numCols * solution.RowIndexes.Count());
+        }
 
         private static Gen<int[,]> GenMatrixOfIntWithNoSolutions()
         {
-            // TODO: It would be easier to generate random rows and then nuke an entire column of the matrix to 0s
-
-            var genZeroOrOne = Any.ValueIn(0, 1);
-            var zeroList = new[] {0}.ToList();
-
             return
                 from numCols in Any.IntBetween(2, 100)
-                from numRows in Any.IntBetween(2, 500)
-                let rowOfAllZeros = Enumerable.Repeat(0, numCols).ToList()
-                let genRowsA = Any.Value(rowOfAllZeros)
+                from numRows in Any.IntBetween(2, 200)
                 from indexOfAlwaysZeroColumn in Any.IntBetween(0, numCols - 1)
-                let prefixLength = indexOfAlwaysZeroColumn
-                let suffixLength = numCols - indexOfAlwaysZeroColumn - 1
-                let genRowsB =
-                    from prefixList in genZeroOrOne.MakeListOfLength(prefixLength)
-                    from suffixList in genZeroOrOne.MakeListOfLength(suffixLength)
-                    select prefixList.Concat(zeroList).Concat(suffixList).ToList()
-                let genRows = Any.WeighedGeneratorIn(
-                    new WeightAndValue<Gen<List<int>>>(90, genRowsA),
-                    new WeightAndValue<Gen<List<int>>>(10, genRowsB))
-                from rows in genRows.MakeListOfLength(numRows)
+                let genZeroOrOne = Any.ValueIn(0, 1)
+                let genRow = genZeroOrOne.MakeListOfLength(numCols).Select(r =>
+                {
+                    r[indexOfAlwaysZeroColumn] = 0;
+                    return r;
+                })
+                from rows in genRow.MakeListOfLength(numRows)
                 select rows.To2DArray();
         }
 
         private static Gen<int[,]> GenMatrixOfIntWithSingleSolution()
         {
             return
-                //from numCols in Any.IntBetween(2, 100)
-                from numCols in Any.IntBetween(2, 20)
+                from numCols in Any.IntBetween(2, 100)
                 from numPieces in Any.IntBetween(1, Math.Min(5, numCols))
                 from partialSolutionRows in GenPartialSolutionRows(numCols, numPieces)
                 // TODO: need to generate a mix of 90% rows of all 0s, 10% partial solution rows
@@ -154,26 +108,25 @@ namespace DlxLibPropertyTests
                 select pieceIndexLists.Select(selectedIdxs => MakePartialSolutionRow(numCols, selectedIdxs)).ToList();
         }
 
-        private static Gen<List<List<int>>> RecursiveHelper(
-            IList<int> remainingPieceLengths,
-            int[] remainingIdxs)
+        private static Gen<List<List<int>>> GenPieceIndexLists(int numCols, IEnumerable<int> pieceLengths)
+        {
+            var remainingIdxs = Enumerable.Range(0, numCols).ToList();
+            // TODO: Could we do this with a call to pieceLengths.Aggregate() instead ?
+            return GenPieceIndexListsRecursiveHelper(pieceLengths.ToList(), remainingIdxs);
+        }
+
+        private static Gen<List<List<int>>> GenPieceIndexListsRecursiveHelper(IList<int> remainingPieceLengths, IList<int> remainingIdxs)
         {
             if (!remainingPieceLengths.Any()) return Any.Value(new List<List<int>>());
 
             var pieceLength = remainingPieceLengths.First();
 
             return
-                from selectedIdxs in GenExtensions.PickValues(pieceLength, remainingIdxs)
-                let newRemainingIdxs = RemoveSelectedIdxs(remainingIdxs, selectedIdxs).ToArray()
-                from rest in RecursiveHelper(remainingPieceLengths.Skip(1).ToList(), newRemainingIdxs)
-                let x = new[] {selectedIdxs}.ToList()
-                select x.Concat(rest).ToList();
-        }
-
-        private static Gen<List<List<int>>> GenPieceIndexLists(int numCols, IEnumerable<int> pieceLengths)
-        {
-            var remainingIdxs = Enumerable.Range(0, numCols).ToArray();
-            return RecursiveHelper(pieceLengths.ToList(), remainingIdxs);
+                from selectedIdxs in GenExtensions.PickValues(pieceLength, remainingIdxs.ToArray())
+                let newRemainingIdxs = RemoveSelectedIdxs(remainingIdxs, selectedIdxs).ToList()
+                let head = new[] { selectedIdxs }.ToList()
+                from tail in GenPieceIndexListsRecursiveHelper(remainingPieceLengths.Skip(1).ToList(), newRemainingIdxs)
+                select head.Concat(tail).ToList();
         }
 
         private static List<int> MakePartialSolutionRow(int numCols, IEnumerable<int> selectedIdxs)
@@ -183,24 +136,14 @@ namespace DlxLibPropertyTests
             return partialSolutionRow;
         }
 
-        private static IEnumerable<int> RemoveSelectedIdxs(IEnumerable<int> remainingIdxs, IEnumerable<int> selectedIdxs)
-        {
-            return remainingIdxs.Except(selectedIdxs);
-        }
-
         //private static Gen<int[,]> GenMatrixOfIntWithMultipleSolutions(int n)
         //{
         //    return Any.OfType<int[,]>();
         //}
 
-        private static void Dump(string format, params object[] args)
+        private static IEnumerable<int> RemoveSelectedIdxs(IEnumerable<int> remainingIdxs, IEnumerable<int> selectedIdxs)
         {
-            Console.WriteLine(format, args);
-        }
-
-        private static string SequenceToString<T>(IEnumerable<T> xs)
-        {
-            return "[" + string.Join(",", xs.Select(x => Convert.ToString(x))) + "]";
+            return remainingIdxs.Except(selectedIdxs);
         }
     }
 }
