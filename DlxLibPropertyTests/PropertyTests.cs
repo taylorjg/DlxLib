188bd243aeae73178f0e9c3e10d3c44d7cf2300d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-02-28 12:29:04 +0000

    Cleanup re generating random matrices with a given number of solutions

--- DlxLibPropertyTests/PropertyTests.cs
+++ DlxLibPropertyTests/PropertyTests.cs
@@ -72,14 +72,6 @@ namespace DlxLibPropertyTests
             Check.One(Config, property);
         }
 
-        [Test]
-        public void Test()
-        {
-            var arb = Arb.fromGen(GenMatrixOfIntWithMultipleSolutions(2));
-            var property = Prop.forAll(arb, FSharpFunc<int[,], Property>.FromConverter(matrix => Prop.ofTestable(true)));
-            Check.One(Config, property);
-        }
-
         private static Property CheckSolutions(IEnumerable<Solution> solutions, int[,] matrix)
         {
             return PropExtensions.AndAll(solutions.Select(solution => CheckSolution(solution, matrix)).ToArray());
@@ -143,82 +135,78 @@ namespace DlxLibPropertyTests
         {
             return
                 from numCols in Any.IntBetween(2, 20)
-                from numSolutionRows in Any.IntBetween(1, Math.Min(5, numCols))
-                from numRows in Any.IntBetween(numSolutionRows, 20)
+                from solution in GenSolution(numCols)
+                from numRows in Any.IntBetween(solution.Count, solution.Count * 5)
                 from matrix in Any.Value(0).MakeListOfLength(numCols).MakeListOfLength(numRows)
-                from solutionRows in GenSolutionRows(numCols, numSolutionRows)
-                from randomRowIdxs in GenExtensions.PickValues(numSolutionRows, Enumerable.Range(0, numRows))
-                select PokeSolutionRowsIntoMatrix(matrix, solutionRows, randomRowIdxs).To2DArray();
+                from randomRowIdxs in GenExtensions.PickValues(solution.Count, Enumerable.Range(0, numRows))
+                select PokeSolutionRowsIntoMatrix(matrix, solution, randomRowIdxs).To2DArray();
         }
 
         private static Gen<int[,]> GenMatrixOfIntWithMultipleSolutions(int numSolutions)
         {
             return
-                from randomPartLengthOld in Any.IntBetween(1, 5)
-                let randomPartLength = 4
+                from randomPartLength in Any.IntBetween(1, 5)
                 let numCols = randomPartLength*numSolutions
-                let partitions =
-                    Enumerable.Range(0, numSolutions)
-                        .Select(x => Tuple.Create(randomPartLength*x, randomPartLength*(x + 1)))
+                let partitions = MakePartitions(numSolutions, randomPartLength)
                 from solutions in GenPartitionedSolutions(numCols, partitions)
                 let combinedSolutions = CombineSolutions(solutions)
-                //from numRows in Any.IntBetween(combinedSolutions.Count, combinedSolutions.Count * 2)
-                //from matrix in Any.Value(0).MakeListOfLength(numCols).MakeListOfLength(numRows)
-                //from randomRowIdxs in GenExtensions.PickValues(combinedSolutions.Count, Enumerable.Range(0, numRows))
-                //select PokeSolutionRowsIntoMatrix(matrix, combinedSolutions, randomRowIdxs).To2DArray();
-                select combinedSolutions.To2DArray();
+                from numRows in Any.IntBetween(combinedSolutions.Count, combinedSolutions.Count * 2)
+                from matrix in Any.Value(0).MakeListOfLength(numCols).MakeListOfLength(numRows)
+                from randomRowIdxs in GenExtensions.PickValues(combinedSolutions.Count, Enumerable.Range(0, numRows))
+                select PokeSolutionRowsIntoMatrix(matrix, combinedSolutions, randomRowIdxs).To2DArray();
         }
 
-        //private static Gen<List<List<int>>> GenSolution(int numCols)
-        //{
-        //    return GenPartitionedSolution(numCols, 0, numCols);
-        //}
-
-        private static Gen<List<List<int>>> GenSolutionRows(int numCols, int numSolutionRows)
+        private static IEnumerable<Tuple<int, int>> MakePartitions(int numSolutions, int randomPartLength)
         {
-            return GenPartitionedSolutionRows(numCols, 0, numCols, numSolutionRows);
+            return Enumerable
+                .Range(0, numSolutions)
+                .Select(partitionIdx => MakePartition(partitionIdx, randomPartLength));
+        }
+
+        private static Tuple<int, int> MakePartition(int partitionIdx, int randomPartLength)
+        {
+            return Tuple.Create(randomPartLength*partitionIdx, randomPartLength*(partitionIdx + 1));
         }
 
         private static Gen<List<List<List<int>>>> GenPartitionedSolutions(int numCols, IEnumerable<Tuple<int, int>> partitions)
         {
             var seed = Any.Value(Enumerable.Empty<List<List<int>>>());
-            var v1 = partitions.Aggregate(seed, (acc, partition) =>
-            {
-                var startIdx = partition.Item1;
-                var endIdx = partition.Item2;
 
-                return (
-                    from list in acc
-                    from v3 in GenPartitionedSolution(numCols, startIdx, endIdx)
-                    let _ = Dump("Inside Aggregate func - numCols: {0}; startIdx: {1}; endIdx: {2}", numCols, startIdx, endIdx)
-                    select list.Concat(new[] {v3}));
-            });
-            return v1.Select(x => x.ToList());
+            return partitions.Aggregate(
+                seed,
+                (acc, partition) =>
+                {
+                    var startIdx1 = partition.Item1;
+                    var endIdx1 = partition.Item2;
+
+                    return (
+                        from solutions in acc
+                        from v3 in GenPartitionedSolution(numCols, startIdx1, endIdx1)
+                        select solutions.Concat(new[] {v3}));
+                },
+                acc => acc.Select(solutions => solutions.ToList()));
+        }
+
+        private static Gen<List<List<int>>> GenSolution(int numCols)
+        {
+            return GenPartitionedSolution(numCols, 0, numCols);
         }
 
         private static Gen<List<List<int>>> GenPartitionedSolution(int numCols, int startIdx, int endIdx)
         {
             return
-                from numSolutionRowsOld in Any.IntBetween(1, Math.Min(5, endIdx - startIdx))
-                let numSolutionRows = 2
+                from numSolutionRows in Any.IntBetween(1, Math.Min(5, endIdx - startIdx))
                 from solutionRows in GenPartitionedSolutionRows(numCols, startIdx, endIdx, numSolutionRows)
                 select solutionRows;
         }
 
         private static Gen<List<List<int>>> GenPartitionedSolutionRows(int numCols, int startIdx, int endIdx, int numSolutionRows)
         {
-            var prefixPartLength = startIdx;
-            var randomPartLength = endIdx - startIdx;
-            var suffixPartLength = numCols - endIdx;
-
             return
-                from prefixPart in Any.Value(1).MakeListOfLength(prefixPartLength)
-                from randomPart in Any.Value(0).MakeListOfLength(randomPartLength)
-                from suffixPart in Any.Value(1).MakeListOfLength(suffixPartLength)
                 from firstSolutionRow in GenRow(numCols, startIdx, endIdx, true)
                 from otherSolutionRows in GenRow(numCols, startIdx, endIdx, false).MakeListOfLength(numSolutionRows - 1)
                 let solutionRows = new[] {firstSolutionRow}.Concat(otherSolutionRows).ToList()
-                from randomRowIdxPerColumn in Any.IntBetween(0, numSolutionRows - 1).MakeListOfLength(randomPartLength)
+                from randomRowIdxPerColumn in Any.IntBetween(0, numSolutionRows - 1).MakeListOfLength(endIdx - startIdx)
                 select RandomlySprinkleOnesIntoSolutionRows(solutionRows, startIdx, randomRowIdxPerColumn);
         }
 
@@ -234,7 +222,6 @@ namespace DlxLibPropertyTests
                 from randomPart in Any.Value(0).MakeListOfLength(randomPartLength)
                 from suffixPart in Any.Value(fillerValue).MakeListOfLength(suffixPartLength)
                 let row = prefixPart.Concat(randomPart).Concat(suffixPart).ToList()
-                let _ = DumpSequence(row) 
                 select row;
         }
 
@@ -258,17 +245,6 @@ namespace DlxLibPropertyTests
             var fromIdx = 0;
             foreach (var toIdx in randomRowIdxs) matrix[toIdx] = solutionRows[fromIdx++];
             return matrix;
-        }
-
-        private static object DumpSequence(IEnumerable<int> sequence)
-        {
-            return Dump("[" + string.Join(",", sequence.Select(x => Convert.ToString(x))) + "]");
-        }
-
-        private static object Dump(string format, params object[] args)
-        {
-            Console.WriteLine(format, args);
-            return null;
         }
     }
 }
