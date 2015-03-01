b9635c46e90829602f5e6d6ee586c6874bbcc27c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-03-01 21:41:49 +0000

    Translated the first property test to F#

---
+++ DlxLibPropertyTestsFs/PropertyTestsFs.fs
@@ -0,0 +1,39 @@
+﻿module DlxLibPropertyTestsFs.PropertyTestsFs
+
+open FsCheck
+open Gen
+open Arb
+open Prop
+open NUnit.Framework
+open DlxLib
+
+let config = Config.VerboseThrowOnFailure
+
+let to2DArray (jagged: _ list list) =
+    let numRows = jagged.Length
+    let numCols = (List.nth jagged 0).Length
+    Array2D.init numRows numCols (fun r c -> List.nth (List.nth jagged r) c)
+
+let genMatrixOfIntWithNoSolutions = 
+    gen {
+        let! numCols = choose (2, 20)
+        let! numRows = choose (2, 20)
+        let! indexOfAlwaysZeroColumn = choose (0, numCols - 1)
+        let genZeroOrOne = elements([0; 1])
+        let genBefore = listOfLength indexOfAlwaysZeroColumn genZeroOrOne
+        let genZero = listOfLength 1 (constant 0)
+        let genAfter = listOfLength (numCols - indexOfAlwaysZeroColumn - 1) genZeroOrOne
+        let genRow =  sequence [genBefore; genZero; genAfter] |> map (fun xs -> xs |> Seq.concat |> Seq.toList)
+        let! rows = listOfLength numRows genRow
+        return to2DArray rows
+    }
+
+[<Test>]
+let ``exact cover problems with no solutions``() =
+    let body = fun matrix ->
+        let dlx = new Dlx()
+        let solutions = dlx.Solve matrix |> Seq.toList
+        solutions.Length = 0 |@ sprintf "Expected no solutions but got %d" solutions.Length
+    let arbMatrix = fromGen genMatrixOfIntWithNoSolutions
+    let property = forAll arbMatrix body
+    Check.One (config, property)
