18c41f5dc19470ba01364187cdaf44322119b013
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-03-01 23:48:51 +0000

    Started work on the second property test in F#

--- DlxLibPropertyTestsFs/PropertyTestsFs.fs
+++ DlxLibPropertyTestsFs/PropertyTestsFs.fs
@@ -1,6 +1,7 @@
 ﻿module DlxLibPropertyTestsFs.PropertyTestsFs
 
 open FsCheck
+open FsCheckUtils
 open Gen
 open Arb
 open Prop
@@ -19,13 +20,73 @@ let genMatrixOfIntWithNoSolutions =
         let! numCols = choose (2, 20)
         let! numRows = choose (2, 20)
         let! indexOfAlwaysZeroColumn = choose (0, numCols - 1)
-        let genZeroOrOne = elements([0; 1])
+        let genZeroOrOne = elements [0; 1]
         let genBefore = listOfLength indexOfAlwaysZeroColumn genZeroOrOne
         let genZero = listOfLength 1 (constant 0)
         let genAfter = listOfLength (numCols - indexOfAlwaysZeroColumn - 1) genZeroOrOne
         let genRow =  sequence [genBefore; genZero; genAfter] |> map (fun xs -> xs |> Seq.concat |> Seq.toList)
         let! rows = listOfLength numRows genRow
-        return to2DArray rows
+        return rows |> to2DArray
+    }
+
+let genRow numCols startIdx endIdx isFirstRow =
+    gen {
+        let fillerValue = if isFirstRow then 1 else 0
+        let prefixPartLength = startIdx
+        let randomPartLength = endIdx - startIdx
+        let suffixPartLength = numCols - endIdx
+        let! prefixPart = listOfLength prefixPartLength (constant fillerValue)
+        let! randomPart = listOfLength randomPartLength (constant 0)
+        let! suffixPart = listOfLength suffixPartLength (constant fillerValue)
+        let row = [prefixPart; randomPart; suffixPart] |> Seq.concat |> Seq.toList
+        return row
+    }
+
+let randomlySprinkleOnesIntoSolutionRows solutionRows startIdx randomRowIdxPerColumn =
+    // var colIndex = startIdx;
+    // foreach (var randomRowIdx in randomRowIdxPerColumn) solutionRows[randomRowIdx][colIndex++] = 1;
+    // return solutionRows;
+    let mutable colIndex = startIdx
+    Seq.iter (fun randomRowIdx -> ()) randomRowIdxPerColumn
+    colIndex <- colIndex + 1
+    solutionRows
+
+let pokeSolutionRowsIntoMatrix matrix solution randomRowIdxs =
+    // var fromIdx = 0;
+    // foreach (var toIdx in randomRowIdxs) matrix[toIdx] = solutionRows[fromIdx++];
+    // return matrix;
+    let mutable fromIndex = 0
+    fromIndex <- fromIndex + 1
+    matrix
+
+let genPartitionedSolutionRows numCols startIdx endIdx numSolutionRows =
+    gen {
+        let! firstSolutionRow = genRow numCols startIdx endIdx true
+        let! otherSolutionRows = genRow numCols startIdx endIdx false |> listOfLength (numSolutionRows - 1)
+        let solutionRows = [Seq.singleton firstSolutionRow |> Seq.toList; otherSolutionRows] |> Seq.concat |> Seq.toList
+        let! randomRowIdxPerColumn = choose (0, numSolutionRows - 1) |> listOfLength (endIdx - startIdx)
+        // TODO: suchThat (fun idxs -> Seq.forall (fun x -> Seq.exists (fun y -> x = y) idxs) (seq { 0..numSolutionRows - 1}))
+        return randomlySprinkleOnesIntoSolutionRows solutionRows startIdx randomRowIdxPerColumn
+    }
+
+let genPartitionedSolution numCols startIdx endIdx =
+    gen {
+        let! numSolutionRows = choose (1, min 5 (endIdx - startIdx))
+        let! solutionRows = genPartitionedSolutionRows numCols startIdx endIdx numSolutionRows
+        return solutionRows
+    }
+
+let genSolution numCols =
+    genPartitionedSolution numCols 0 numCols
+
+let genMatrixOfIntWithSingleSolution = 
+    gen {
+        let! numCols = choose (2, 20)
+        let! solution = genSolution numCols
+        let! numRows = choose (solution.Length, solution.Length * 5)
+        let! matrix = constant 0 |> listOfLength numCols |> listOfLength numRows
+        let! randomRowIdxs = GenExtensions.PickValues(solution.Length, seq { 0..numRows - 1})
+        return pokeSolutionRowsIntoMatrix matrix solution randomRowIdxs |> to2DArray
     }
 
 [<Test>]
